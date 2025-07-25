371c87e3700a2259c0141fe885783361038fbfc2
Kevin Kim <kimanikevin254@gmail.com>
2025-07-25 09:52:21 +0300

    enable cors

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
+from flask_cors import CORS
 from app import create_app
 
 app = create_app(os.getenv('FLASK_ENV', 'development'))
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
